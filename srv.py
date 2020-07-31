f00e1b62cbc6568c9baa067fcffad46f7a4edbef
Pchel11 <ipchel61@gmail.com>
2020-07-31 20:09:42 +0300

    xxx

--- srv.py
+++ srv.py
@@ -8,7 +8,19 @@ print(PORT)
 
 
 class MyHandler(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        content = """
+        <html>
+        <head><title>XXX</title><head>
+        <body>Hello world!</body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Conten-length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode())
 
 
 if __name__ == "__main__":
