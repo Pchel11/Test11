a55e92c56380f8cbd8505b22bfecf2e8a0b6b378
Pchel11 <ipchel61@gmail.com>
2020-08-04 17:37:29 +0300

    xxx

--- srv.py
+++ srv.py
@@ -50,10 +50,8 @@ class MyHttp(SimpleHTTPRequestHandler):
 
         return result
 
+
 if __name__ == "__main__":
     with socketserver.TCPServer(("", settings.PORT), MyHttp) as httpd:
         print("it works")
         httpd.serve_forever(poll_interval=1)
-
-
-
