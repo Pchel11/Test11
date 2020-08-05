02aad896274b15c659bf7195cd54c2edbb87f87b
Pchel11 <ipchel61@gmail.com>
2020-08-05 19:22:27 +0300

    xxx'

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import socketserver
 import settings
 from srv import MyHttp
 
-__path__ = self.build_path
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", settings.PORT), MyHttp) as httpd:
