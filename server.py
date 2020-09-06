18cdb7e86101f4f7ae05bd5b08d8058e73867050
Pchel11 <ipchel61@gmail.com>
2020-09-06 16:27:57 +0300

    xxx

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import traceback
-from datetime import datetime
+from datetime import date
 from http.server import SimpleHTTPRequestHandler
 
-import settings
+from consts import CSS_CLASS_ERROR
+from consts import USERS_DATA
 from custom_types import HttpRequest
+from custom_types import User
 from errors import MethodNotAllowed
 from errors import NotFound
-from settings import STORAGE_DIR
-from utils import get_user_data
 from utils import read_static
 from utils import to_bytes
+from utils import to_str
 
 
 class MyHttp(SimpleHTTPRequestHandler):
@@ -51,37 +52,14 @@ class MyHttp(SimpleHTTPRequestHandler):
         payload = payload_in_bytes.decode()
         return payload
 
-    def handle_hello(self, request):
+    def handle_hello(self, request: HttpRequest):
         if request.method != "get":
             raise MethodNotAllowed
 
-        query_string = self.get_user_qs_from_file()
+        query = self.load_user_data()
+        user = User.build(query)
 
-        user = get_user_data(query_string)
-
-        year = datetime.now().year - int(user.age)
-
-        content = f"""
-        <html>
-        <head><title>Hello Page</title></head>
-        <body style="background-color:00BFFBF">
-        <h1>Hello {user.name}!</h1>
-        <h2>You were born in {year}</h2>
-        <p>path: {self.path}</p>
-            
-            <form method="post" action="/hello-update">
-                <div class="name"><label for="name-id">Your name:</label>
-                <input type="text" name="name" id="name-id">
-                </div>
-                <div class="age" style="position:absolute;top:180;"><label for="year-id">Your age:</label>
-                <input type="text" name="age" id="age-id">
-                <button type="submit" id="greet_button-id" style="position:absolute;left:250;top:-33;height:5em">Greet</button>
-                </div>           
-            </form>
-
-        </body>
-        </html>
-        """
+        content = self.render_hello_page(user, user)
 
         self.respond(content)
 
@@ -89,12 +67,67 @@ class MyHttp(SimpleHTTPRequestHandler):
         if request.method != "post":
             raise MethodNotAllowed
 
-        qs = self.get_request_payload()
-        self.save_user_qs_to_file(qs)
-        self.redirect("/hello")
+        form_data = self.get_form_data()
+        new_user = User.build(form_data)
 
-    def handle_zde(self):
+        if not new_user.errors:
+            self.save_user_data(form_data)
+            self.redirect("/hello")
+            return
+
+        saved_data = self.load_user_data()
+        saved_user = User.build(saved_data)
+
+        hello_page = self.render_hello_page(new_user, saved_user)
+
+        self.respond(hello_page)
+
+    @staticmethod
+    def render_hello_page(new_user: User, saved_user: User) -> str:
+        css_class_for_name = css_class_for_age = ""
+        label_for_name = "Your name: "
+        label_for_age = "Your age: "
+
+        age_new = age_saved = saved_user.age
+        name_new = name_saved = saved_user.name
+
+        year = date.today().year - age_saved
+
+        if new_user.errors:
+            if "name" in new_user.errors:
+                error = new_user.errors["name"]
+                label_for_name = f"ERROR: {error}"
+                css_class_for_name = CSS_CLASS_ERROR
+
+            if "age" in new_user.errors:
+                error = new_user.errors["age"]
+                label_for_age = f"ERROR: {error}"
+                css_class_for_age = CSS_CLASS_ERROR
+
+            name_new = new_user.name
+            age_new = new_user.age
+
+        template = read_static("hello.html").decode()
+
+        context = {
+            "age_new": age_new or "",
+            "label_for_age": label_for_age,
+            "label_for_name": label_for_name,
+            "name_new": name_new or "",
+            "name_saved": name_saved or "",
+            "class_for_age": css_class_for_age,
+            "class_for_name": css_class_for_name,
+            "year": year,
+        }
+
+        content = template.format(**context)
+
+        return content
+
+    @staticmethod
+    def handle_zde():
         x = 1 / 0
+        print(x)
 
     def handle_static(self, file_path, ct):
         content = read_static(file_path)
@@ -110,26 +143,6 @@ class MyHttp(SimpleHTTPRequestHandler):
     def handle_500(self):
         self.respond(traceback.format_exc(), code=500, content_type="text/plain")
 
-    @staticmethod
-    def get_user_qs_from_file():
-        qs_file = STORAGE_DIR / "xxx.txt"
-        if not qs_file.is_file():
-            return ""
-
-        with qs_file.open("r") as src:
-            content = src.read()
-
-        if isinstance(content, bytes):
-            content = content.decode()
-
-        return content
-
-    @staticmethod
-    def save_user_qs_to_file(query: str):
-        qs_file = STORAGE_DIR / "xxx.txt"
-        with qs_file.open("w") as dst:
-            dst.write(query)
-
     def respond(self, message, code=200, content_type="text/html"):
         payload = to_bytes(message)
 
@@ -144,3 +157,31 @@ class MyHttp(SimpleHTTPRequestHandler):
         self.send_header("Location", to)
         self.end_headers()
 
+    def get_form_data(self) -> str:
+        content_length_as_str = self.headers.get("content-length", 0)
+        content_length = int(content_length_as_str)
+
+        if not content_length:
+            return ""
+
+        payload_as_bytes = self.rfile.read(content_length)
+        payload = to_str(payload_as_bytes)
+
+        return payload
+
+    @staticmethod
+    def load_user_data() -> str:
+        if not USERS_DATA.is_file():
+            return ""
+
+        with USERS_DATA.open("r") as src:
+            data = src.read()
+
+        data = to_str(data)
+
+        return data
+
+    @staticmethod
+    def save_user_data(data: str) -> None:
+        with USERS_DATA.open("w") as dst:
+            dst.write(data)
