1e3f708b52e3b13121d4d773ade8fc501f72179b
Pchel11 <ipchel61@gmail.com>
2020-11-11 19:02:18 +0300

    xxx

--- src/applications/blog/models.py
+++ src/applications/blog/models.py
@@ -1,14 +1,23 @@
 from delorean import now
 from django.db import models
+from django.urls import reverse_lazy
+
+created_at_default = now().datetime
 
 
 class Post(models.Model):
     # xxx = models.TextField(unique=True)
-    title = models.TextField(unique=True)
-    content = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(default=lambda: now().datetime)
+    title = models.CharField(unique=True, max_length=100)
+    content = models.CharField(null=True, blank=True, max_length=5000)
+    created_at = models.DateTimeField(default=created_at_default)
     visible = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse_lazy("blog:post", kwargs={"pk": self.pk})
 
     def __str__(self):
         msg = f"'{self.title}', visible? {self.visible}"
         return msg
+
+    class Meta:
+        ordering = ["-created_at", "title", "pk"]
--- src/applications/blog/urls.py
+++ src/applications/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from applications.blog.apps import BlogConfig
-from applications.blog.views import IndexView
+from applications.blog.views import IndexView, NewPostView, DeletePostView, UpdatePostView
 
 app_name = BlogConfig.label
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("new/", NewPostView.as_view(), name="new-post"),
+    path("<int:pk>/delete/", DeletePostView.as_view(), name="delete-post"),
+    path("<int:pk>/update/", UpdatePostView.as_view(), name="update-post"),
 ]
